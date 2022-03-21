01673e30b52d228595032f3166e9a0aba968f2da
Giulio Dille Girardi <Giulio.Girardi@edu.pucrs.br>
2022-03-21 20:45:37 -0300

    Inicia desafio da mochila com divisao e conquista

---
+++ DesafioMochila.java
@@ -0,0 +1,34 @@
+/**
+Resolva o problema da mochila conforme o enuciado em sala de aula.
+
+Ache uma solução que testa todas as combinações possíveis e seleciona a melhor, aplicando divisão-e-conquista ou não;
+Contabilize o número de iterações;
+Implemente e teste sua solução, para o caso exposto em aula e outros de mesmo porte (;-)).
+
+Teste:
+    Capacidade: 165
+    Pesos:  23, 31, 29, 44, 53, 38, 63, 85, 89, 82
+    Valores: 92, 57, 49, 68, 60, 43, 67, 84, 87, 72
+    Blocos selecionados: 1, 2, 3, 4, 6
+
+
+    vetorDiferenca[] = (capacidade-totalPesos)<=menor;
+ */
+
+ public class DesafioMochila{
+
+
+     public int[] desafioDivConqui(int valores[], int pesos[], int capacidade){
+         1 2
+            3
+         2
+         3
+
+     }
+
+     public static void main(String args[]){
+
+     }
+
+
+ }
